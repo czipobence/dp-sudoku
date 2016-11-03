e9543393f59a816f42a1bdb8fe07cf17a25f3417
Bence Czipó <czipobence@gmail.com>
2016-11-03 14:45:31 +0100

    fa tukorkepe

--- Gyak/7Gyak/orai.erl
+++ Gyak/7Gyak/orai.erl
@@ -5,3 +5,6 @@
 
 fa_noveltje(level) -> level;
 fa_noveltje({N,F1,F2}) -> {N+1,fa_noveltje(F1),fa_noveltje(F2)}.
+
+fa_tukorkepe(level) -> level;
+fa_tukorkepe({N,R,L}) -> {N,fa_tukorkepe(L),fa_tukorkepe(R)}.
