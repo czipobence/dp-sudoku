e749e4122e1ed3ded513583f1714adeab0e44d06
Bence Czipó <czipobence@gmail.com>
2016-11-03 14:49:28 +0100

    fa jobb es balerteke

--- Gyak/7Gyak/orai.erl
+++ Gyak/7Gyak/orai.erl
@@ -7,4 +7,12 @@ fa_noveltje(level) -> level;
 fa_noveltje({N,F1,F2}) -> {N+1,fa_noveltje(F1),fa_noveltje(F2)}.
 
 fa_tukorkepe(level) -> level;
-fa_tukorkepe({N,R,L}) -> {N,fa_tukorkepe(L),fa_tukorkepe(R)}.
+fa_tukorkepe({N,L,R}) -> {N,fa_tukorkepe(R),fa_tukorkepe(L)}.
+
+fa_balerteke(level) -> error;
+fa_balerteke({N,level,_}) -> {ok, N};
+fa_balerteke({_,L,_}) -> fa_balerteke(L).
+
+fa_jobberteke(level) -> error;
+fa_jobberteke({N,_,level}) -> {ok, N};
+fa_jobberteke({_,_,R}) -> fa_jobberteke(R).
