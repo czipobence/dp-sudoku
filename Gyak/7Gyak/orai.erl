1dddfeea61907a57d10c3ad80d6edac335922c18
Bence Czipó <czipobence@gmail.com>
2016-11-03 15:00:42 +0100

    rendezett fa

--- Gyak/7Gyak/orai.erl
+++ Gyak/7Gyak/orai.erl
@@ -16,3 +16,17 @@ fa_balerteke({_,L,_}) -> fa_balerteke(L).
 fa_jobberteke(level) -> error;
 fa_jobberteke({N,_,level}) -> {ok, N};
 fa_jobberteke({_,_,R}) -> fa_jobberteke(R).
+
+rendezett_fa(level) -> true;
+rendezett_fa({_,level,level}) -> true;
+rendezett_fa({N,L,level}) ->
+	{ok, JE} = fa_jobberteke(L), 
+	rendezett_fa(L) and JE < N;
+rendezett_fa({N,level,R}) ->
+	{ok, BE} = fa_balerteke(R),
+	rendezett_fa(R) and BE > N;
+rendezett_fa({N,L,R}) ->
+	{ok, BE} = fa_balerteke(R),
+	{ok, JE} = fa_jobberteke(L),
+	(JE < N) andalso (BE > N) andalso 
+	rendezett_fa(L) andalso rendezett_fa(R).
