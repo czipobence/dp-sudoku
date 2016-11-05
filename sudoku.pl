6cca9b6a1bda01f4ad89c82fdedcd12d1c119f8d
Bence Czipó <czipobence@gmail.com>
2016-11-05 17:23:44 +0100

    binding variables

--- sudoku.pl
+++ sudoku.pl
@@ -11,7 +11,31 @@
 % :- pred sudoku(sspec::in, ssol::out).
 sudoku(s(K,Bo),Sol) :-
 	preprocess(Bo,BoPP),
-	allowed(K,BoPP,1,Sol).
+	allowed(K,BoPP,1,All),
+	set_value(K,All,(2,2,3,[]),Sol).
+
+% :- type fieldinfo ---> (int,int,int,list(info))
+% set_value(K,V,(R,C,N,I),V1):
+% V1 a V, K parameteru megoldashalmaz szukitese az R. sor C. oszlop    
+% N-re rogzitesevel V infok mellett
+set_value(K,V,FI,V1) :-
+	set_value(K,V,FI,1,[],V1R),
+	reverse(V1R,V1).
+
+set_value(_,[],_,_,Acc,Acc).
+set_value(K,[HV|TV],FI,Rc,Acc,V1) :-
+	set_value_row(K,HV,FI,Rc,1,[],HUR),
+	reverse(HUR,HU),
+	Rc1 is Rc + 1,
+	set_value(K,TV,FI,Rc1, [HU|Acc] ,V1).
+
+set_value_row(_,[],_,_,_,Acc,Acc).
+set_value_row(K,[H|T],(R,C,N,I),Rc,Cc,Acc,HU) :-
+	Cc1 is Cc + 1,
+	(	R = Rc, C = Cc -> PF = N
+	;	PF = H
+	),
+	set_value_row(K,T,(R,C,N,I),Rc,Cc1,[PF | Acc],HU).
 
 allowed(K,_,R,[]) :- R is K*K + 1.
 allowed(K,Bo,R,[H|T]) :-
@@ -40,7 +64,8 @@ preprocess(PreR,[CurrR|T],Acc,PP) :-
 %preprocess_row(_,[],Acc,Acc).
 preprocess_row([],[CurrI], Acc, [CurrI|Acc]).
 preprocess_row([],[CurrI,NextI|CurrT],Acc,PP) :-
-	(	member(w,NextI) -> preprocess_row([],[NextI|CurrT], [[a|CurrI]|Acc],PP)
+	(	member(w,NextI) -> 
+			preprocess_row([],[NextI|CurrT], [[a|CurrI]|Acc],PP)
 	;	preprocess_row([],[NextI|CurrT], [CurrI|Acc],PP)
 	).
 preprocess_row([PreI],[CurrI],Acc,PP) :-
@@ -67,13 +92,14 @@ preprocess_row([PreI|PreT], [CurrI,NextI|CurrT], Acc, PP) :-
 %    (a) 1..k*k közötti egész, ahol k az SSpec feladvány cellamérete,
 %    (b) teljesíti az adott mezőre vonatkozó szám- és paritási infók
 %        által előírt megszorításokat, továbbá
-%    (c) különbözik az adott mezőt tartalmazó sor, oszlop és cella többi
-%        mezőjében szereplő száminfóktól, 
+%    (c) különbözik az adott mezőt tartalmazó sor, oszlop és cella 
+%        többi mezőjében szereplő száminfóktól, 
 % ahol
 %    SSpec az sspec típusspecifikációnak megfelelő Sudoku-feladvány,
-%    R_C az adott feladvány egy mezőjének (sor-oszlop formában megadott) koordinátája,
-%    Vals list(int) típusú mezőértéklista, az SSpec feladvány R_C koordinátájú
-%         mezőjében megengedett értékek listája. 
+%    R_C az adott feladvány egy mezőjének (sor-oszlop formában 
+%    megadott) koordinátája, Vals list(int) típusú mezőértéklista, az 
+%    SSpec feladvány R_C koordinátájú mezőjében megengedett értékek
+%    listája. 
 ertekek(s(K,Mx), R-C, Vals) :-
 	findall(N, ertekek(Mx,R,C,K,N), Vals).
 
@@ -96,7 +122,7 @@ mx_item(Mx,R,C,Val) :-
 	list_nth(Mx,R,TMP),
 	list_nth(TMP,C,Val).
 
-%list_nth(L,N,Val)
+%list_nth(L,N,Val):
 %Val L lista N. eleme (1tol kezdve a szamozast)
 list_nth([H|_],1,H).
 list_nth([_|T],N,Val):-
@@ -104,21 +130,21 @@ list_nth([_|T],N,Val):-
 	N1 is N-1,
 	list_nth(T,N1,Val).
 
-%in_col(Mx,N,Item)
+%in_col(Mx,N,Item):
 %Item megtalalhato Mx matrix N. oszlopaban (1tol kezdve)
 in_col([H|T],N,Item) :-
 	list_nth(H,N,Item);
 	in_col(T,N,Item).
 
-%negyzet(Mx,R,C,K,Sq)
-%Sq Mx, K parameteru sudoku R. soraban es C. oszlopaban talalhato elemet bennfoglalo negyzet
+% negyzet(Mx,R,C,K,Sq): Sq Mx, K parameteru sudoku R. soraban es C. 
+% oszlopaban talalhato elemet bennfoglalo negyzet
 negyzet(Mx,R,C,K,Sq) :-
 	SR is R - ((R - 1) mod K),
 	SC is C - ((C - 1) mod K),
 	submatrix(Mx,SR,SC,K,K,Sq).
 
-%remove_mx_element(Mx,R,C,M1)
-%M1 Mx matrix, aminek R. soranak C. oszlopaban levo elemet []-re csereltek
+%remove_mx_element(Mx,R,C,M1): M1 Mx matrix, aminek R. soranak C. 
+% oszlopaban levo elemet []-re csereltek
 remove_mx_element([H|T1],R,C,[H|T2]) :-
 	R > 1,
 	R1 is R-1,
@@ -126,7 +152,7 @@ remove_mx_element([H|T1],R,C,[H|T2]) :-
 remove_mx_element([H1|T],1,C,[H2|T]):-
 	remove_list_element(H1,C,H2).
 
-%remove_list_element(L,N,L1)
+%remove_list_element(L,N,L1):
 %L1 L lista N. elemenek []-re cserelesevel kapott lista
 remove_list_element([H|T1],C,[H|T2]):-
 	C > 1,
@@ -134,8 +160,8 @@ remove_list_element([H|T1],C,[H|T2]):-
 	remove_list_element(T1,C1,T2).
 remove_list_element([_|T],1,[[]|T]).
 
-%limited_to(V,K,N)
-% N olyan szám, amely V info mellett egy K parameteru sudoku adott mezojeben lehet
+%limited_to(V,K,N): N olyan szám, amely V info mellett egy K parameteru
+% sudoku adott mezojeben lehet
 limited_to([H|T],K,N) :-
 	limited_to(H,K,N),
 	limited_to(T,K,N).
@@ -149,9 +175,9 @@ limited_to(V,K,N) :-
 	; (V = s; V = w; V = n; V = a; V = [])
 	).
 
-%allowed_by(V,K,N)
-%N olyan szam ami egy K parameteru sudokuban V infoval azonos szegmensben
-%azaz sorban, oszlopban vagy negyzetben, elofordulhat.
+%allowed_by(V,K,N) :N olyan szam ami egy K parameteru sudokuban V 
+% infoval azonos szegmensben azaz sorban, oszlopban vagy negyzetben,
+% elofordulhat.
 allowed_by([H|T],K,N) :-
 	allowed_by(H,K,N),
 	allowed_by(T,K,N).
@@ -162,17 +188,14 @@ allowed_by(V,K,N) :-
 	 (V = s; V = w; V = n; V = a; V=e; V=o; V=[])
 	).
 
-%even(N)
-%N paros szam
+%even(N): N paros szam
 even(N):-
 	0 is N mod 2.
-%odd(N)
-%N paratlan szam
+%odd(N): N paratlan szam
 odd(N):-
 	1 is N mod 2.
 
-%bet(From,To,N)
-%N From es To kozotti szam
+%bet(From,To,N): N From es To kozotti szam
 bet(From,To,N) :-
 	From =< To, N = From.
 bet(From,To,N) :-
@@ -189,15 +212,16 @@ submatrix(Mx, StartR, StartC, Height, Width, Smx) :-
 	sublist(Mx, StartR, Height, Lt),
 	cutAll(Lt, StartC, Width, Smx).
 
-% cutAll(Mx, StartR, Height, M) M mátrix Mx olyan részmátrixa, mely annak
-% Height sorát tartalmazza sorfolytonosan a StartR. sortól kezdve	
+% cutAll(Mx, StartR, Height, M): M mátrix Mx olyan részmátrixa, mely 
+% annak Height sorát tartalmazza sorfolytonosan a StartR. sortól kezdve	
 cutAll([], _, _, []).
 cutAll([H|T], StartR, Height, [HSmx|TSmx]) :-
 	isList(H),
 	sublist(H,StartR,Height, HSmx),
 	cutAll(T, StartR, Height, TSmx).
 
-% sublist(L, S, N, L1): Az L1 lista az L lista N hosszúságú részlistája az S. elemmel kezdve
+% sublist(L, S, N, L1): Az L1 lista az L lista N hosszúságú részlistája
+% az S. elemmel kezdve
 sublist(L, S, N, L1) :-
 	S1 is S -1,
 	drop(L,S1,Ltemp),
@@ -211,8 +235,8 @@ take([H|T], N, [H | Taken]) :-
 	N1 is N-1,
 	take(T, N1, Taken).
 
-% drop(L0, N, L): Az L0 lista olyan szuffixuma L, amely az L0 elsõ N elemét
-%% nem tartalmazza
+% drop(L0, N, L): Az L0 lista olyan szuffixuma L, amely az L0 elsõ N 
+% elemét nem tartalmazza
 drop([], _, []).
 drop([H|T], 0, [H|T]).
 drop([_|T], N, Dropped) :-
@@ -220,6 +244,6 @@ drop([_|T], N, Dropped) :-
 	N1 is N-1,
 	drop(T,N1,Dropped).
 
-% isList(L) L egy tetszőleges lista (annak eldöntésére, hogy L lista-e)
+%isList(L): L egy tetszőleges lista (annak eldöntésére, hogy L lista-e)
 isList([]).
 isList([_|_]).
